b6b31d893d3b57fc5ed1d34662ba52bc71070588
Adnan Naushahi <adnan.naushahi@hotmail.com>
2020-05-18 11:26:34 +0100

    checkout interface and implementation class added

---
+++ Kata/Implementation/Checkout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kata.Interface;
+
+namespace Kata.Implementation
+{
+    class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Total()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kata/Interface/ICheckout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Interface
+{
+    interface ICheckout
+    {
+        void Scan(string item);
+        decimal Total(); 
+    }
+}
